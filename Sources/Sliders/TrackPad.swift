58d1c1ffec541f46757612bd6604a590ad480fd4
khoi <git@khoi.io>
2020-11-01 15:48:21 +0700

    Revert "Reset to .zero when released TrackPad"
    This reverts commit 2504cc31a090d053f7a7983966b55458a5d41e75.

--- Sources/Sliders/TrackPad.swift
+++ Sources/Sliders/TrackPad.swift
@@ -30,8 +30,6 @@ public struct TrackPadConfiguration {
     public let minY: Double
     /// The maximum value from rangeY
     public let maxY: Double
-    
-    public let defaultPos: CGPoint?
 }
 // MARK: - Style
 @available(iOS 13.0, macOS 10.15, watchOS 6.0 , *)
@@ -267,7 +265,7 @@ public struct TrackPad: View {
                                     self.isActive = true
                                 })
                                 .onEnded({
-                                    self.value = .zero
+                                    self.constrainValue(proxy, $0.location)
                                     self.isActive = false
                                 }))
 
